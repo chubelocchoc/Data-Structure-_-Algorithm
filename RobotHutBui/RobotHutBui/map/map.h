f1e7f28778590e006423bf5001b474b0c4dcc0fe
Chu Be Loc Choc <20520034@gm.uit.edu.vn>
2022-07-01 23:53:27 +0700

    first add

---
+++ RobotHutBui/RobotHutBui/map/map.h
@@ -0,0 +1,221 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct CELL
+{
+	int x = -1; // row index
+	int y = -1; // column index
+	int type = -1; // 0.Robot, 1.Obstacle, 2.Cleaned, 3.UnCleaned, 4. Wall
+};
+
+struct Node
+{
+	CELL cell;
+	Node* pNorth = NULL;
+	Node* pEast = NULL;
+	Node* pWest = NULL;
+	Node* pSouth = NULL;
+};
+
+Node* CreateNode(int status)
+{
+	Node* p = new Node;
+	p->cell.x = -1;
+	p->cell.y = -1;
+	p->cell.type = status;
+	p->pNorth = NULL;
+	p->pEast = NULL;
+	p->pWest = NULL;
+	p->pSouth = NULL;
+	return p;
+}
+
+class Map
+{
+protected:
+	int rows;
+	int cols;
+	Node* pHead;			//Luu lai Node dau tien
+
+	Node* pPivotHead;		//Luu lai Node dau tien cua mot dong
+	Node* pTail;			//Luu lai Node sau cung hien tai
+public:
+	Node* getpHead()
+	{
+		return pHead;
+	}
+	void KhoiTaoBanDo(int n_rows, int n_cols)
+	{
+		rows = n_rows;
+		cols = n_cols;
+		pHead = NULL;
+		pTail = NULL;
+	}
+
+	// Robot (x), Obstackle (*), Cleaned (.), UnCleaned ( ), Wall (-) 
+	void Insert(Node* p)
+	{
+		if (pHead == NULL)
+		{
+			p->cell.x = 0;
+			p->cell.y = 0;
+			pHead = p;
+			pTail = p;
+			pPivotHead = pHead;
+		}
+		else
+		{
+			if (pTail->cell.y != cols - 1)		//Truong hop hang hien tai chua day
+			{
+				p->cell.x = pTail->cell.x ;
+				p->cell.y = pTail->cell.y + 1;
+				if (pTail->cell.x == 0)		 //Neu dang insert hang dau tien khong can ket noi voi node phia Bac 		
+				{
+
+					p->pWest = pTail;
+					pTail->pEast = p;
+					pTail = p;
+				}
+				else	//Neu insert hang tiep theo phai quan tam Node phia Bac
+				{
+					Node* pCurrentNorth  = pTail->pNorth;
+					pCurrentNorth = pCurrentNorth->pEast; //Chuyen pPivotNorth sang Node phia Bac node muon chen vao
+					p->pNorth = pCurrentNorth;			
+					p->pWest = pTail;
+					pCurrentNorth->pSouth = p;
+					pTail->pEast = p;
+					pTail = p;
+				}
+			}
+			else	//Truong hop Hang hien tai da du so node
+			{
+				//Tao Node dau hang moi
+				p->pNorth = pPivotHead;
+				p->cell.y = 0;
+				p->cell.x = pPivotHead->cell.x + 1;
+				pPivotHead->pSouth = p;
+				pTail = p;
+				pPivotHead = p;		//Luu lai Node dau hang moi tao vao pPivotHead
+
+			}
+		}
+	}
+	void Insert(int x)
+	{
+		Node* p = CreateNode(x);
+		Insert(p);
+	}
+
+	void ShowMap()
+	{
+		Node* p1 = pHead;
+		while (p1)
+		{
+			Node* p2 = p1;
+			while (p2)
+			{
+				switch (p2->cell.type)
+				{
+				case 0:
+					cout << "x";
+					break;
+				case 1:
+					cout << "*";
+					break;
+				case 2:
+					cout << ".";
+					break;
+				case 3:
+					cout << " ";
+					break;
+				case 4:
+					cout << "=";
+					break;
+				default:
+					break;
+				}
+				cout << " ";
+				p2 = p2->pEast;
+			}
+			cout << endl;
+			p1 = p1->pSouth;
+		}
+	}
+
+	Node* FindNode(int n_x, int n_y)
+	{
+		Node* p = pHead;
+		while (p->cell.x != n_x)
+		{
+			p = p->pSouth;
+		}
+		while (p->cell.y != n_y)
+		{
+			p = p->pEast;
+		}
+		return p;
+	}
+
+	void Nhap(string filename)		//Nhap ma tran tu file .txt, cac phan tu trong so do phan biet nhau boi ki tu tab
+	{
+		string line;
+		ifstream myfile;
+		myfile.open(filename);
+
+		if (myfile.is_open())
+		{
+			myfile >> rows;
+			myfile >> cols;
+			KhoiTaoBanDo(rows, cols);
+			while (getline(myfile, line))	//tien hanh duyet tung dong du lieu trong file .txt
+			{
+				string line_s = line;		//chuyen dong du lieu sang kieu string
+				for (int i = 0; i < line_s.size(); i++)
+				{
+					if ((int)line_s[i] != 9)		//kiem tra co phai ki tu "tab"
+					{
+						int temp = line_s[i] - '0';
+						Insert(temp);
+					}
+				}
+			}
+		}
+	}
+
+
+	void XoaMap()
+	{
+		if (pHead != NULL)
+		{
+			Node* p1 = pHead;
+			while (true)
+			{
+				Node* p2 = p1->pEast;
+				while (p2)
+				{
+					Node* p_del = p2;
+					p2 = p2->pEast;
+					delete p_del;
+				}
+				if (p1->pSouth != NULL)
+				{
+					p1 = p1->pSouth;
+				}
+				else
+				{
+					break;
+				}
+			}
+			p1 = p1->pNorth;
+			while (p1)
+			{
+				Node* p_del = p1;
+				p1 = p1->pNorth;
+				delete p_del;
+			}
+		}
+	}
+};
---
+++ RobotHutBui/RobotHutBui/robot/robot.h
@@ -0,0 +1,571 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "../map/map.h"
+using namespace std;
+
+struct Direction
+{
+	Node* position = NULL;
+	int direction = -1;
+};
+
+class Cleaner
+{
+    protected:
+        string manufacturer = "";
+        string serial_number = "";
+        float current_battery = 0;
+		float battery_threshold = 0;
+        Map map_data;
+		Direction current;
+		
+		int uncleaned_direction = 4;
+
+    public:
+		void Menu()
+		{
+			int option = -1;
+			do
+			{
+				cout << "Chon mot trong hai che do: 1. Manual, 2. Auto, 3. Reset, 4. Exit: ";
+				cin >> option;
+				switch (option)
+				{
+				case 1:
+					Manual();
+					break;
+				case 2:
+					Auto();
+					break;
+				case 3:
+					Reset();
+					break;
+				default:
+					break;
+				}
+			} while (option != 4);
+			cout << "Ket thuc chuong trinh" << endl;
+		}
+
+		void Reset()
+		{
+			Map room_map;
+			cout << "Chinh sua ban do tren file input.txt... Nhan bat ki phim nao de tiep tuc chuong trinh." << endl;;
+			system("pause");
+			room_map.Nhap("input.txt");
+			cout << "Da nhap xong du lieu ban do. Ban do hien tai:" << endl;
+			room_map.ShowMap();
+
+			map_data.XoaMap();
+			SetMap(room_map);
+			cout << "Da dua thong tin ban do vao robot. Thiet lap vi tri robot:" << endl;
+			SetRobotInMap();
+		}
+	
+		//NHAP va XUAT
+		void Init()
+		{
+			cout << "Nhap manufacturer: ";
+			getline(cin, manufacturer);
+			cout << "Nhap serial number: ";
+			getline(cin, serial_number);
+			cout << "Nhap dung luong pin toi da: ";
+			cin >> battery_threshold;
+			cout << "Nhap dung luong pin hien tai: ";
+			cin >> current_battery;
+
+		}
+
+		void SetMap(Map input)
+		{
+			this->map_data = input;
+		}
+
+		void SetRobotInMap()
+		{
+			int x, y;
+			cout << "Nhap toa do x cua may hut bui (bat dau tu 0, tu tren xuong duoi): ";
+			cin >> x;
+			cout << "Nhap toa do y cua may hut bui (bat dau tu 0, tu trai qua phai): ";
+			cin >> y;
+			current.position = map_data.FindNode(x, y);
+
+			cout << "Nhap huong cua may hut bui (0.North, 1.East, 2.South, 3.West): ";
+			cin >> current.direction;
+
+			current.position->cell.type = 0;
+		}
+
+
+		void PrintInfo()
+		{
+			cout << "*Manufacturer: " << manufacturer << ". Serial Number: " << serial_number << endl;
+			cout << "- Current postion: " << current.position->cell.x << "," << current.position->cell.y << endl;
+			cout << "- Current direction: ";
+			switch (current.direction)
+			{
+			case 0:
+				cout << "North.";
+				break;
+			case 1:
+				cout << "East.";
+				break;
+			case 2:
+				cout << "South.";
+				break;
+			case 3:
+				cout << "West.";
+				break;
+			default:
+				break;
+			}
+			cout << endl;
+			cout << "- Current battery: " << GetBattery() << "/" << battery_threshold << endl;
+			cout << "- Current map: " << endl;
+			map_data.ShowMap();
+			ControlLowBattery();
+		}
+
+		//BATTERY CONTROL
+		//Thiet lap dung luong pin toi da
+		void SetBatteryThreshold(float threshold)
+		{
+			this->battery_threshold = threshold;
+		}
+
+		//Kiem tra pin
+		bool CheckBattery(float threshold)
+		{
+			if (current_battery <= threshold)
+				return false;
+			else
+				return true;
+		}
+
+		//Thong bao khi pin yeu
+		void ControlLowBattery()
+		{
+			float temp = battery_threshold;
+			int flag = 0;
+			if (!CheckBattery(float(0.05)*temp) && (float(0.05)*temp < (current_battery + 1)))
+			{
+				cout << "LUU Y: Nguon pin duoi 5%. Xin hay sac robot cua ban." << endl;
+				flag = 1;
+			}
+			else
+			{
+				if (!CheckBattery(float(0.1)*temp) && (float(0.1)*temp < (current_battery + 1)))
+				{
+					cout << "LUU Y: Nguon pin duoi 10%. Xin hay sac robot cua ban." << endl;
+					flag = 1;
+				}
+				else
+				{
+					if (!CheckBattery(float(0.15)*temp) && (float(0.15)*temp < (current_battery + 1)))
+					{
+						cout << "LUU Y: Nguon pin duoi 15%. Xin hay sac robot cua ban." << endl;
+						flag = 1;
+					}
+				}
+			}
+			if (flag == 1)
+			{
+				cout << "Tiep tuc? (y/n): ";
+				char ch = 'n';
+				cin >> ch;
+				if (ch == 'n')
+				{
+					exit(1);
+				}
+			}
+		}
+
+		//Lay gia tri pin hien tai
+		float GetBattery()
+		{
+			return this->current_battery;
+		}
+
+		
+
+		//CAC PHUONG THUC TIM DUONG
+		void SetDirection(int x, int y, int direction)          // Chuyen vi tri va huong cua may hut bui
+		{
+			this->current.position->cell.x = x;
+			this->current.position->cell.y = y;
+			this->current.direction = direction;
+		}
+
+		Direction* CheckNextPosition(int direction, bool allowCleaned = false) // kiem tra vi tri va huong tiep theo
+		{
+			Direction* result = new Direction;
+			*result = current;
+			int cell_check;
+
+			//Gan x, y va huong cua vi tri moi vao result
+			switch (direction)
+			{
+			case 0: // North
+				result->position = result->position->pNorth;
+				result->direction = 0;
+				break;
+			case 1: // East
+				result->position = result->position->pEast;
+				result->direction = 1;
+				break;
+			case 2: // South
+				result->position = result->position->pSouth;
+				result->direction = 2;
+				break;
+			case 3: // West
+				result->position = result->position->pWest;
+				result->direction = 3;
+				break;
+			default:
+				cout << "ERROR" << endl;
+				break;
+
+			}
+			//Lay tu ban do kiem tra vi tri do la loai nao
+			cell_check = result->position->cell.type;
+			// Obstacle or Wall . Neu vi tri do la tuong hoac chuong ngai vat thi tra ve NULL
+			if (cell_check == 1 || cell_check == 4)
+				return NULL;
+			// Cleaned. Neu hien tai o che do khong cho phep di qua vi tri da don va o do da don thi tra ve NULL
+			else
+				if (!allowCleaned && cell_check == 2)
+					return NULL;
+				else
+					return result;
+			return NULL;
+		}
+
+		bool FindPath(bool isAllowCleaned) {
+			int instruction[4][4] = { {0,1,3,2},{1,2,0,3},{2,3,1,0},{3,0,2,1} };      //Cac bo so lan luot can nhac cac huong
+			int direction;
+			for (int i = 0; i < 4; i++)
+			{
+				direction = instruction[current.direction][i];
+				Direction* res = CheckNextPosition(direction, isAllowCleaned);    //isAllowCleaned = true cho phep di o da don, false la khong
+				if (res)
+				{
+					this->current = *res;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		bool CheckUncleanedOnDirection(Direction result) //Kiem tra huong hien tai co o nao trong hay khong, co thi luu lai vao uncleaned_direction
+		{
+			Node* p = result.position;
+			switch (result.direction)
+			{
+			case 0: // North			
+				while (p != NULL)
+				{
+					if (p->cell.type == 3)
+					{
+						this->uncleaned_direction = 0;
+						return true;
+					}
+					p = p->pNorth;
+				}
+				break;
+			case 1: // East
+				while (p != NULL)
+				{
+					if (p->cell.type == 3)
+					{
+						this->uncleaned_direction = 1;
+						return true;
+					}
+					p = p->pEast;
+				}
+				break;
+			case 2: // South
+				while (p != NULL)
+				{
+					if (p->cell.type == 3)
+					{
+						this->uncleaned_direction = 2;
+						return true;
+					}
+					p = p->pSouth;
+				}
+				break;
+			case 3: // West
+				while (p != NULL)
+				{
+					if (p->cell.type == 3)
+					{
+						this->uncleaned_direction = 3;
+						return true;
+					}
+					p = p->pNorth;
+				}
+				break;
+			}
+			return false;
+		}
+
+		bool FindPath_Auto()
+		{
+			int instruction[4][4] = { {0,1,3,2},{1,2,0,3},{2,3,1,0},{3,0,2,1} };      //Cac bo so lan luot can nhac cac huong
+			int direction;
+			for (int i = 0; i < 4; i++)
+			{
+				direction = instruction[current.direction][i];
+				Direction* res = CheckNextPosition(direction, false);    //isAllowCleaned = true cho phep di o da don, false la khong
+				if (res != NULL)
+				{
+					this->current = *res;
+					return true;
+				}
+			}
+			
+			for (int i = 0; i < 4; i++)
+			{
+				direction = instruction[current.direction][i];
+				Direction* res = CheckNextPosition(direction, true);    //isAllowCleaned = true cho phep di o da don, false la khong
+				if (res != NULL && CheckUncleanedOnDirection(*res))
+				{
+					this->current = *res;
+					return true;
+				}
+			}
+				
+			for (int i = 0; i < 4; i++)
+			{
+				direction = instruction[current.direction][i];
+				Direction* res = CheckNextPosition(direction, true);    //isAllowCleaned = true cho phep di o da don, false la khong
+
+				if (res != NULL)
+				{
+
+					this->current = *res;
+					if (this->uncleaned_direction < 4)
+					{
+						this->current.direction = this->uncleaned_direction;
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		
+		//MANUAL MODE
+		//Chon hanh dong cho robot
+		int ChooseAction(unsigned int flag)
+		{
+			cout << "Choose action:" << endl;
+			cout << "1.GoAhead" << "\n2.TurnRight" << "\n3.GoBack" << "\n4.TurnLeft" << "\n0.Exit" << endl;
+			cin >> flag;  //Nhap vao hanh dong
+			if (flag == 0)
+				return 0;
+			else if (flag < 5)
+				return flag;
+			else
+			{
+				cout << "Choose Action again!" << endl;
+				return ChooseAction(flag);
+			}
+		}
+
+		void RotateRight90()
+		{
+			current.direction++;
+			if (current.direction == 4)
+			{
+				current.direction = 0;
+			}
+		}
+
+		void RotateLeft90()
+		{
+			current.direction--;
+			if (current.direction == -1)
+			{
+				current.direction = 3;
+			}
+		}
+
+		void Rotate180()
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				RotateRight90();
+			}
+		}
+
+		//Doi huong robot theo hanh dong da chon
+		void UpdateDirection(unsigned int flag)
+		{
+			switch (flag) //hanh dong
+			{
+			case 1:  //chon di thang
+				break;
+			case 2:  //chon turn right
+			{
+				RotateRight90();
+				break;
+			}
+			case 3:  //chon turn back
+			{
+				Rotate180();
+				break;
+			}
+			case 4:  //chon turn left
+			{
+				RotateLeft90();
+				break;
+			}
+			}
+		}
+
+		void RoBotGo()
+		{
+			ControlLowBattery();
+			if (current_battery < 0.02*battery_threshold)
+			{
+				cout << "Het pin. Khong the tien hanh di chuyen. Xin hay sac." << endl;
+				return;
+			}
+
+			current.position->cell.type = 0;
+			current_battery--;
+		}
+
+		void LiveControl()
+		{
+			unsigned int temp = 1;
+			while (true)
+			{
+				temp = ChooseAction(temp);
+				UpdateDirection(temp);
+				if (temp == 0)
+					break;
+				current.position->cell.type = 2;
+				bool kq_findpath = FindPath(true);
+				if (kq_findpath)
+					RoBotGo();
+				PrintInfo();
+			}
+		}
+
+		void SetupControl(int n, int flag)
+		{
+			UpdateDirection(flag);
+			for (int i = 0; i < n; i++)
+			{
+				current.position->cell.type = 2;
+				bool kq_findpath = FindPath(true);
+				if (kq_findpath)
+					RoBotGo();
+				PrintInfo();
+				Sleep(500);
+			}
+		}
+
+		void Manual()
+		{
+			PrintInfo();
+			
+			int option = -1;
+			do
+			{
+				cout << "Chon: 1. Live Control, 2. Setup Control, 0.Exit: ";
+				cin >> option;
+				switch (option)
+				{
+				case 1:
+					LiveControl();
+					break;
+				case 2:
+				{
+					int step;
+					unsigned int flag = 1;
+					cout << "Nhap so buoc de robot di chuyen: ";
+					cin >> step;
+					flag = ChooseAction(flag);
+					SetupControl(step, flag);
+					break;
+				}
+				default:
+					break;
+				}
+			} while (option != 0);
+		}
+
+
+		//AUTOMATIC MODE
+
+		int CountUncleaned()
+		{
+			int count_uncleaned = 0;
+			Node* p1 = map_data.getpHead();
+			while (p1)
+			{
+				Node* p2 = p1;
+				while (p2)
+				{
+					if (p2->cell.type == 3)
+						count_uncleaned++;
+					p2 = p2->pEast;
+				}
+				p1 = p1->pSouth;
+			}
+			return count_uncleaned;
+		}
+
+		void Auto()
+		{
+			int timer;
+			timer = 0;
+			int count_uncleaned = CountUncleaned();
+			cout << "*Timer: " << timer << endl;
+			PrintInfo();
+			Sleep(500);
+
+			timer++;
+			while (count_uncleaned != 0)
+			{
+				
+				cout << "*Timer: " << timer << endl;
+				if (current_battery > 0.02*battery_threshold)
+				{
+					timer++;
+					Node* p = current.position;
+					FindPath_Auto();
+					p->cell.type = 2; //Danh dau tren map o truoc do da don sach
+					if (current.position->cell.type == 3)
+					{
+						count_uncleaned--;
+					}
+					current.position->cell.type = 0; //Danh dau tren map vi tri hien tai cua robot
+					current_battery--;
+					PrintInfo();
+					Sleep(500);
+				}
+				else
+				{
+					cout << "Het pin. Khong the tien hanh di chuyen. Xin hay sac." << endl;
+					return;
+				}
+				
+				cout << "-----------------------" << endl;
+				
+			}
+			if (CountUncleaned() == 0)
+			{
+				cout << "DA DON SACH! :)" << endl;
+			}
+			else
+			{
+				cout << "ERROR" << endl;
+			}
+		}
+};
